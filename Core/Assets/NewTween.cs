9af0c7dceabbc8a2c875d102db4aae37acb73c4d
Gustavo Garcia <gustavogs@hyperbeard.com>
2019-02-02 00:58:24 -0600

    First implementation of new Tween

---
+++ Core/Assets/NewTween.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum NewTweenStopAction {
+	CompleteInterpolationMakeFinishCallback,
+	MakeFinishCallback,
+	Nothing
+}
+
+public class NewTween : MonoBehaviour {
+
+	#region Class members
+	private static GameObject root;
+	private static List<TweenElement> tweens = new List<TweenElement> ();
+	#endregion
+
+	#region Class accesors
+	#endregion
+
+	#region Class overrides
+	#endregion
+
+	#region Class implementation
+	#endregion
+
+	#region Interface implementation
+	#endregion
+}
