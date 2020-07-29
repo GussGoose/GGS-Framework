18c69b3d450a89363a629a7bd9e3b8c764cdd1ee
Gustavo Garcia <gustavogs2811@gmail.com>
2020-07-28 23:07:27 -0500

    Runtime styles path fixed

--- Assets/ThirdParty/GGS_Framework/Scripts/Runtime/Core/GGS_FrameworkStyles.cs
+++ Assets/ThirdParty/GGS_Framework/Scripts/Runtime/Core/GGS_FrameworkStyles.cs
@@ -1,17 +1,11 @@
-﻿#if UNITY_EDITOR
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace GGS_Framework
 {
-	public static class GGS_FrameworkStyles
-	{
-		#region Class Accesors
-		public static GUISkin Skin
-		{
-			get { return EditorGUIUtility.Load ("UtilityFramework/UtilityFrameworkGUISkin.guiskin") as GUISkin; }
-		}
-		#endregion
-	}
+    public static class GGS_FrameworkStyles
+    {
+        #region Accesors
+        public static readonly GUISkin Skin = Resources.Load<GUISkin> ("RuntimeEditorResources/Runtime_GUISkin");
+        #endregion
+    }
 }
-#endif
